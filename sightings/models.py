27eb932e7a80b0c104847eb312d0701342bbf0d4
xiaojin201908 <57262342+xiaojin201908@users.noreply.github.com>
2019-12-04 02:34:07 -0500

    Update models.py

--- sightings/models.py
+++ sightings/models.py
@@ -6,9 +6,9 @@ from django.utils.translation import gettext as _
 class sighting(models.Model):
     Latitude = models.FloatField(help_text=_('Latitude'),
                                max_length=10,)
-    Longitude = models.FloatField(help_text=_('Latitude'),
+    Longitude = models.FloatField(help_text=_('Longitude'),
                                max_length=10,)
-    Unique_Squirrel_ID = models.IntegerField(primary_key=True) 
+    Unique_Squirrel_ID = models.IntegerField(max_length = 20, primary_key=True,) 
     def __str__(self):
       return self.Unique_Squirrel_ID
 

14702de35605d3b3ca7589bfe1031627970ff2e7
Xinyao Wang <xw2675@columbia.edu>
2019-12-05 16:21:08 +0000

    html files ok

--- sightings/urls.py
+++ sightings/urls.py
@@ -1,6 +1,11 @@
-
 from django.urls import path
 from . import views
+
+app_name = 'squirrel'
 urlpatterns = [
-        path('', views.index, name = 'index'),
+        path('',views.main_list,name='main_list'),
+        path('add/',views.add, name='add'),
+        path('stats/',views.stats,name='stats'),
+        path('<str:unique_squirrel_id>/', views.update,name='update'),
+        path('<str:unique_squirrel_id>/delete', views.delete, name='delete'),
         ]
--- squirrel/settings.py
+++ squirrel/settings.py
@@ -1,3 +1,4 @@
+
 """
 Django settings for squirrel project.
 
@@ -31,6 +32,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'sightings.apps.SightingsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',

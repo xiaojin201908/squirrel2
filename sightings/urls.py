d3d395ef17f18d14b50ef92d7b059ff78f758761
ellen26 <56944883+ellen26@users.noreply.github.com>
2019-12-06 21:12:56 -0500

    Update urls.py

--- sightings/urls.py
+++ sightings/urls.py
@@ -3,9 +3,8 @@ from . import views
 
 app_name = 'squirrel'
 urlpatterns = [
-        path('',views.main_list,name='main_list'),
+        path('',views.all_list,name='all_list'),
         path('add/',views.add, name='add'),
         path('stats/',views.stats,name='stats'),
         path('<str:unique_squirrel_id>/', views.update,name='update'),
-        path('<str:unique_squirrel_id>/delete', views.delete, name='delete'),
         ]

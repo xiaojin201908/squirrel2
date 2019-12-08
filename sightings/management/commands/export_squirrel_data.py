289654837a1a52f7f4b79e6d90342e25757d2f9e
xiaojin201908 <57262342+xiaojin201908@users.noreply.github.com>
2019-12-07 22:14:33 -0500

    Update export_squirrel_data.py

--- sightings/management/commands/export_squirrel_data.py
+++ sightings/management/commands/export_squirrel_data.py
@@ -3,16 +3,25 @@ import os
 import csv
 from sightings.models import Squirrel
 import datetime
+
+
 class Command(BaseCommand):
     def add_arguments(self,parser):
-        parser.add_argument('csvfile', nargs='+',type=str)
+        parser.add_argument('csvfile')
 
     def handle(self, *args, **options):
-        path = options['csvfile'][0]
+        path = options['csvfile']
+        fields = Squirrel._meta.fields
+        with open(path, 'w') as csvFile:
+            writer = csv.writer(csvFile)
+            for object in Squirrel.objects.all():
+                line =[]
+                for field in fields:
+                    line.append(getattr(object, field.name))
+                writer.writerow(row)
+                    
+                    
 
-        with open(path, 'w', newline='') as csvFile:
-            varlist = [v for v in vars(Squirrel.objects.all()[0]).keys() if v not in ['_state', 'id']]
-            writer = csv.writer(csvFile, delimiter=',')
-            writer.writerow([Squirrel._meta.get_field(v).help_text for v in varlist])
-            for instance in Squirrel.objects.all():
-                writer.writerow([getattr(instance, v) for v in varlist])
+                
+                
+                
